6489719155548b915aae325ace3fd915f21709c1
Aaron <76023256+Aaron193@users.noreply.github.com>
2023-03-19 16:09:17 -0400

    piece position randomized not touching grid

--- src/PuzzlePiece.ts
+++ src/PuzzlePiece.ts
@@ -1,10 +1,29 @@
 import { Constants } from './Constants';
-import { GRID_SIZE } from './Game';
+import { GRID_SIZE, HEIGHT, WIDTH } from './Game';
 import { Sprite } from './Sprite';
 import { Vector2 } from './types/Vector';
 
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
+}
+
+function getRandomPiecePosition() {
+    const width = Constants.canvas.width;
+    const gridX = width * 0.5 - WIDTH * GRID_SIZE * 0.5;
+
+    const height = Constants.canvas.height;
+    const gridY = height * 0.5 - HEIGHT * GRID_SIZE * 0.5;
+
+    let x, y;
+
+    // no piece can be this close to the grid
+    let padding = GRID_SIZE;
+    do {
+        x = randomNumber(padding, width - padding);
+        y = randomNumber(padding, height - padding);
+    } while (x + padding > gridX && x - padding < gridX + WIDTH * GRID_SIZE && y + padding > gridY && y - padding < gridY + HEIGHT * GRID_SIZE);
+
+    return { x, y };
 }
 
 export class PuzzlePiece {
@@ -15,10 +34,7 @@ export class PuzzlePiece {
     constructor(path: string, pieceNumber: number) {
         const spritePath = `${path}/${pieceNumber}.jpg`;
         this.sprite = new Sprite(spritePath, 0.5);
-        this.position = {
-            x: randomNumber(Constants.canvas.width / 2, Constants.canvas.width - GRID_SIZE / 2),
-            y: randomNumber(0, Constants.canvas.height),
-        };
+        this.position = getRandomPiecePosition();
         this.pieceNumber = pieceNumber;
     }
 
