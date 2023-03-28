d504dbd0d12f32d98efd24b12c44c232964945bc
Aaron <76023256+Aaron193@users.noreply.github.com>
2023-03-28 18:04:34 -0400

    es6 imports and dist path

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-const express = require("express");
+import express from 'express';
+
 const app = express();
 let PORT = 6969 || process.env.PORT;
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
     console.log(`Waiting on: ${PORT}`);
